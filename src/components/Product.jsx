810dd4718d0547d23488dbc8cf0b68771a11133e
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-15 23:53:53 +0100

    adjusting text

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -16,7 +16,7 @@ function Product(props) {
         <h2 className="text-2xl font-semibold mt-4">{name}</h2>
         <p className=" p-2">{price} SEK</p>
         {/* <p className=" text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quos veniam veritatis, odio inventore, ullam labore voluptas tempore officia sequi aperiam..... <span className=" font-semibold">more</span> </p> */}
-        <p className=" text-sm">{description.substring(0, 75)+'...'} </p>
+        <p className=" text-sm">{description.substring(0, 80)+'...'} </p>
         <div className="flex text-sm gap-2 mt-6">
         <button className=" btn flex justify-center gap-1"
           onClick={(e) => {
